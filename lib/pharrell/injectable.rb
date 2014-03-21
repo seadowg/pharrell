561c9ead1821b061933088f7be65ae4a554f02c1
Jeff Taggart <jeff@jetaggart.com>
2014-03-20 20:04:40 -0600

    Add simpler injection

--- lib/pharrell/injectable.rb
+++ lib/pharrell/injectable.rb
@@ -5,7 +5,12 @@ module Pharrell
     end
 
     module ClassMethods
-      def injected(name, klass)
+      def injected(name, klass=nil)
+        if klass.nil?
+          klass_name = name.to_s.split("_").map(&:capitalize).join
+          klass = Kernel.const_get(klass_name)
+        end
+
         define_method(name) do
           @__pharrell_cache__ ||= {}
           @__pharrell_cache__[klass] ||= Pharrell.instance_for(klass)
