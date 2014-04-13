32436c22aa52eef81b1b52485bd6633984ca72f3
Callum Stott <callum@seadowg.com>
2014-04-13 17:39:25 +0100

    Add RSpec configure helper for one shot setup

--- lib/pharrell/integration/rspec.rb
+++ lib/pharrell/integration/rspec.rb
@@ -1,6 +1,15 @@
 module Pharrell
   module Integration
     module RSpec
+      def configure(config_name, rspec_config)
+        config.include(Pharrell::Injectable)
+        config.include(Pharrell::Integration::RSpec)
+
+        config.before(:each) do
+          Pharrell.use_config(config_name)
+        end
+      end
+
       def self.included(base)
         base.extend(ClassMethods)
       end
