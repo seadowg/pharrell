4e6dbe6d1aec29351b3ae0a7a998cbc976dedf16
Callum Stott <callum@seadowg.com>
2014-04-22 16:17:40 +0100

    Refactor Pharrell global object to use Environment instance :neckbeard:
    This means that the configuration state is all held within an instance
    that is held in one static reference rather than having multiple static
    variables hanging around. Might allow for a cleaner internal interface and
    a more modular setup.

--- lib/pharrell.rb
+++ lib/pharrell.rb
@@ -4,60 +4,90 @@ require 'pharrell/constructor'
 require 'pharrell/errors'
 
 module Pharrell
-  @@configs = {}
-  @@config = nil
+  @@environment = nil
 
   def self.config(name, opts = {}, &blk)
-    check_options([:extends], opts)
-
-    if opts[:extends]
-      @@configs[name] = fetch_config(opts[:extends]).extend(blk)
-    else
-      @@configs[name] = Config.new(blk)
-    end
+    environment.config(name, opts, &blk)
   end
 
   def self.use_config(name)
-    @@config = name
-    rebuild!
+    environment.use_config(name)
   end
 
   def self.instance_for(klass)
-    current_config.instance_for(klass)
+    environment.instance_for(klass)
   end
 
   def self.rebuild!
-    current_config.rebuild!
+    environment.rebuild!
   end
 
   def self.reset!
-    @@configs = {}
-    @@config = nil
+    environment.reset!
   end
 
   private
 
-  def self.current_config
-    fetch_config(@@config)
+  def self.environment
+    @@enironment ||= Environment.new
   end
 
-  def self.fetch_config(name)
-    config = @@configs[name]
+  class Environment
+    @configs = {}
+    @config = nil
 
-    if config
-      config
-    else
-      raise ConfigNotDefinedError
+    def config(name, opts = {}, &blk)
+      check_options([:extends], opts)
+
+      if opts[:extends]
+        @configs[name] = fetch_config(opts[:extends]).extend(blk)
+      else
+        @configs[name] = Config.new(blk)
+      end
     end
-  end
 
-  def self.check_options(opts, opt_hash)
-    extra_keys = opt_hash.keys.reject { |key| opts.include?(key) }
+    def use_config(name)
+      @config = name
+      rebuild!
+    end
 
-    if extra_keys.empty?
-      true
-    else
-      raise InvalidOptionsError.new(extra_keys)
+    def instance_for(klass)
+      current_config.instance_for(klass)
+    end
+
+    def rebuild!
+      current_config.rebuild!
+    end
+
+    def reset!
+      @configs = {}
+      @config = nil
+    end
+
+    private
+
+    def current_config
+      fetch_config(@config)
+    end
+
+    def fetch_config(name)
+      config = @configs[name]
+
+      if config
+        config
+      else
+        raise ConfigNotDefinedError
+      end
+    end
+
+    def check_options(opts, opt_hash)
+      extra_keys = opt_hash.keys.reject { |key| opts.include?(key) }
+
+      if extra_keys.empty?
+        true
+      else
+        raise InvalidOptionsError.new(extra_keys)
+      end
     end
   end
 end
